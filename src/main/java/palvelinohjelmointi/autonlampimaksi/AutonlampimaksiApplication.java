f92e4bc1056321d6b71dcdeed61decc08ffcaaf7
niikari <pakniiles@gmail.com>
2021-05-20 11:46:48 +0300

    initial commit

---
+++ src/main/java/palvelinohjelmointi/autonlampimaksi/AutonlampimaksiApplication.java
@@ -0,0 +1,13 @@
+package palvelinohjelmointi.autonlampimaksi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonlampimaksiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonlampimaksiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/palvelinohjelmointi/autonlampimaksi/AutonlampimaksiApplicationTests.java
@@ -0,0 +1,13 @@
+package palvelinohjelmointi.autonlampimaksi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonlampimaksiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
